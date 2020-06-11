61bca4f7c728c28694168deec2d886b6ad5d9c90
Jiang Ye <iamyoukou@gmail.com>
2020-06-11 12:32:54 +0900

    test 2d fft

--- src/fft.cpp
+++ src/fft.cpp
@@ -30,52 +30,6 @@ void fft(CArray &x) {
   }
 }
 
-// Cooley-Tukey FFT (in-place, breadth-first, decimation-in-frequency)
-// Better optimized but less intuitive
-// !!! Warning : in some cases this code make result different from not
-// optimased version above (need to fix bug) The bug is now fixed @2017/05/30
-// void fft(CArray &x) {
-//   // DFT
-//   unsigned int N = x.size(), k = N, n;
-//   double thetaT = 3.14159265358979323846264338328L / N;
-//   Complex phiT = Complex(cos(thetaT), -sin(thetaT)), T;
-//   while (k > 1) {
-//     n = k;
-//     k >>= 1;
-//     phiT = phiT * phiT;
-//     T = 1.0L;
-//     for (unsigned int l = 0; l < k; l++) {
-//       for (unsigned int a = l; a < N; a += n) {
-//         unsigned int b = a + k;
-//         Complex t = x[a] - x[b];
-//         x[a] += x[b];
-//         x[b] = t * T;
-//       }
-//       T *= phiT;
-//     }
-//   }
-//   // Decimate
-//   unsigned int m = (unsigned int)log2(N);
-//   for (unsigned int a = 0; a < N; a++) {
-//     unsigned int b = a;
-//     // Reverse bits
-//     b = (((b & 0xaaaaaaaa) >> 1) | ((b & 0x55555555) << 1));
-//     b = (((b & 0xcccccccc) >> 2) | ((b & 0x33333333) << 2));
-//     b = (((b & 0xf0f0f0f0) >> 4) | ((b & 0x0f0f0f0f) << 4));
-//     b = (((b & 0xff00ff00) >> 8) | ((b & 0x00ff00ff) << 8));
-//     b = ((b >> 16) | (b << 16)) >> (32 - m);
-//     if (b > a) {
-//       Complex t = x[a];
-//       x[a] = x[b];
-//       x[b] = t;
-//     }
-//   }
-//   //// Normalize (This section make it not working correctly)
-//   // Complex f = 1.0 / sqrt(N);
-//   // for (unsigned int i = 0; i < N; i++)
-//   //	x[i] *= f;
-// }
-
 // inverse fft (in-place)
 void ifft(CArray &x) {
   // conjugate the complex numbers
@@ -91,24 +45,64 @@ void ifft(CArray &x) {
   x /= x.size();
 }
 
+void fft2(CArray &x) {}
+
 int main() {
-  const Complex test[] = {1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0};
-  CArray data(test, 8);
+  // const Complex test[] = {1, 0, 1, 0, 1, 1, 1, 1};
+  // CArray data(test, 8);
+  //
+  // // forward fft
+  // fft(data);
+  //
+  // std::cout << "fft" << std::endl;
+  // for (int i = 0; i < data.size(); ++i) {
+  //   std::cout << data[i] << std::endl;
+  // }
+  //
+  // // inverse fft
+  // ifft(data);
+  //
+  // std::cout << std::endl << "ifft" << std::endl;
+  // for (int i = 0; i < data.size(); ++i) {
+  //   std::cout << data[i] << std::endl;
+  // }
 
-  // forward fft
-  fft(data);
+  Complex test[4][4] = {{0, 1, 2, 3}, {0, 1, 2, 3}, {0, 1, 2, 3}, {0, 1, 2, 3}};
 
-  std::cout << "fft" << std::endl;
-  for (int i = 0; i < 8; ++i) {
-    std::cout << data[i] << std::endl;
+  // fft for each row
+  for (size_t i = 0; i < 4; i++) {
+    Complex temp[] = {test[i][0], test[i][1], test[i][2], test[i][3]};
+
+    CArray data(temp, 4);
+
+    fft(data);
+
+    test[i][0] = data[0];
+    test[i][1] = data[1];
+    test[i][2] = data[2];
+    test[i][3] = data[3];
   }
 
-  // inverse fft
-  ifft(data);
+  // fft for each column
+  for (size_t i = 0; i < 4; i++) {
+    Complex temp[] = {test[0][i], test[1][i], test[2][i], test[3][i]};
 
-  std::cout << std::endl << "ifft" << std::endl;
-  for (int i = 0; i < 8; ++i) {
-    std::cout << data[i] << std::endl;
+    CArray data(temp, 4);
+
+    fft(data);
+
+    test[0][i] = data[0];
+    test[1][i] = data[1];
+    test[2][i] = data[2];
+    test[3][i] = data[3];
   }
+
+  for (size_t i = 0; i < 4; i++) {
+    std::cout << test[i][0] << ", ";
+    std::cout << test[i][1] << ", ";
+    std::cout << test[i][2] << ", ";
+    std::cout << test[i][3] << '\n';
+  }
+
   return 0;
 }
